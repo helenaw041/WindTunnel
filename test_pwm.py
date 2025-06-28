da8f108321e988b3a8097db2e1aba8553913d1e3
helenaw041 <helenaw041@gmail.com>
2025-06-28 22:13:21 +0000

    test

--- test_pwm.py
+++ test_pwm.py
@@ -1,13 +1,4 @@
 import Adafruit_BBIO.PWM as PWM
 import time
 
-PWM.start("P2_1", 75, 200, 1)  # 50% duty cycle, 1 kHz
-
-# time.sleep(5)
-
-# while(1){   
-#     PWM.set_duty_cycle("P2_01", 75)  # Change duty cycle to 75%
-#     time.sleep(2)
-
-# PWM.stop("P2_01")
-# PWM.cleanup()
+PWM.start("P2_1", 75, 200, 1)  # 25% duty cycle, 200 Hz
